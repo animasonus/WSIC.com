65fb31ddd7281e24a969b34d1e4f1cefe600d822
Noah Edge <soulsound1154@yahoo.com>
2017-12-15 01:11:59 -0500

    Got the suggestion thing to work
    Now that that's aquared away, all I have to do is do the submit box and add more suggestions while working on the advanced tab

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @suggestion_hash = {'Simple Web Server': 'admin', 'Variable Changer': 'admin', 'Text-to-Speech Parser': 'admin', 'Tweet Like Counter': 'admin', 'Fibonacci Sequencer': 'admin', }
   end
 
   def submit
